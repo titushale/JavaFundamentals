1286a159b1b87c596e239bef7238c3bf3c3bd855
Titus Hale <titus.hale15@gmail.com>
2018-10-10 11:17:14 -0500

    This is our first java project

---
+++ Project/src/com/Fundamentals/Java/WelcomeToJava.java
@@ -0,0 +1,10 @@
+package com.Fundamentals.Java;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Java");
+	}
+
+}
